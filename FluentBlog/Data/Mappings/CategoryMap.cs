f1fc511a56daa8797ddb6af73d8a17684180e0a5
gabrielhs1357 <gabrielhs1357@gmail.com>
2022-04-07 22:54:42 -0300

    WIP: Add models and some mappings using Fluent API

---
+++ FluentBlog/Data/Mappings/CategoryMap.cs
@@ -0,0 +1,38 @@
+﻿using FluentBlog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FluentBlog.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            // Antes era [Table("Category")]
+            builder.ToTable("Category");
+
+            // Primary key
+            builder.HasKey(x => x.Id);
+
+            // Identity
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+
+            builder.HasIndex(x => x.Slug, "IX_Category_Slug")
+                .IsUnique();
+        }
+    }
+}
---
+++ FluentBlog/Models/Tag.cs
@@ -0,0 +1,10 @@
+namespace FluentBlog.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public List<Tag> Tags { get; set; }
+    }
+}
